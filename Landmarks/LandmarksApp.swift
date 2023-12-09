5518bc3c3657dd57b350d95024dbbf39a44675dd
Aaron <aaronhma@gmail.com>
2023-12-09 11:40:06 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Aaron Ma on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
